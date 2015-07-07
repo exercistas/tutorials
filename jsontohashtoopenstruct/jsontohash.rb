8a4cfa8574786a054fcd7e906db5c2f72d4e99fb
Mixolidia Gautreaux <mixolidia@gmail.com>
2015-07-07 13:47:59 -0700

    Add bootstrap.js, app.js, template for method.

--- jsontohashtoopenstruct/jsontohash.rb
+++ jsontohashtoopenstruct/jsontohash.rb
@@ -3,6 +3,12 @@ require 'ostruct'
 
 file = File.read('tohash.json')
 data_hash = JSON.parse(file)
-# data_struct = OpenStruct.new(data_hash)
-data_struct = OpenStruct.new(data_hash['tracks'][0])
+data_struct = OpenStruct.new(data_hash)
+# data_struct = OpenStruct.new(data_hash['tracks'][0])
 puts data_struct
+
+def foo_list items
+  content_tag :ul do
+      items.collect {|item| concat(content_tag(:li, item))}
+  end
+end
