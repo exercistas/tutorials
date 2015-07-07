995d3e8257fac20670e53f4dab0ae33753192cc8
Sarah Guermond <s.guermond@gmail.com>
2015-07-06 17:08:09 -0700

    changed how we input the hash into the OpenStruct

--- jsontohashtoopenstruct/jsontohash.rb
+++ jsontohashtoopenstruct/jsontohash.rb
@@ -3,5 +3,5 @@ require 'ostruct'
 
 file = File.read('tohash.json')
 data_hash = JSON.parse(file)
-data_struct = OpenStruct.new(data_hash)
-puts data_struct[1].favoriteFruit
+data_struct = OpenStruct.new(data_hash['tracks'][0])
+puts data_struct
