d8745cee0a4dac95cdd591e0c41f4e6905437435
Mixolidia Gautreaux <mixolidia@gmail.com>
2015-07-06 17:09:17 -0700

    looking for ostruct answers.

--- jsontohashtoopenstruct/jsontohash.rb
+++ jsontohashtoopenstruct/jsontohash.rb
@@ -1,7 +1,8 @@
 require 'json'
 require 'ostruct'
+require 'pp'
 
 file = File.read('tohash.json')
 data_hash = JSON.parse(file)
 data_struct = OpenStruct.new(data_hash)
-puts data_struct[1].favoriteFruit
+puts data_struct
