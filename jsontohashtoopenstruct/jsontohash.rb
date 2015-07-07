b7a744e1174f74a00d48ed3ee7c189f5d723b957
Sarah Guermond <s.guermond@gmail.com>
2015-07-07 14:52:04 -0700

    Loops through OpenStruct to pull out name, slug, and activity

--- jsontohashtoopenstruct/jsontohash.rb
+++ jsontohashtoopenstruct/jsontohash.rb
@@ -4,8 +4,15 @@ require 'ostruct'
 file = File.read('tohash.json')
 data_hash = JSON.parse(file)
 data_struct = OpenStruct.new(data_hash)
-# data_struct = OpenStruct.new(data_hash['tracks'][0])
-puts data_struct
+lang_hash = {}
+
+for lang in 0...data_struct.tracks.length do
+	language = data_struct.tracks[lang]["language"]
+	slug = data_struct.tracks[lang]["slug"]
+	active = data_struct.tracks[lang]["active"]
+	lang_hash[:"#{slug}"] = OpenStruct.new(:language => language, :slug => slug, :active => active)
+end 
+
 
 def foo_list items
   content_tag :ul do
